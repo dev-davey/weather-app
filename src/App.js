fa85317c3e6ba85a5253dcf81ce03064d729bb89
davey <davey89451@gmail.com>
2021-06-09 14:50:32 -0700

    responsive fix

--- src/App.js
+++ src/App.js
@@ -19,6 +19,30 @@ export default class App extends Component {
       error: ""
     }
   }
+
+  componentDidMount(){
+   this.performFirstSearch()
+  }
+
+  performFirstSearch = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=New York,United States&APPID=36dbd4f4d180c0a6ea3f797fe7c0402d`)
+    .then(response => response.json())
+    .then(data => {
+    this.setState({
+      temperature: Math.floor((data.main.temp -273.15) * 1.8 + 32),
+      description: data.weather[0].description,
+      city: data.name,
+      humidity: data.main.humidity,
+      country: data.sys.country,
+      icon: data.weather[0].icon,
+      wind: data.wind.speed
+      })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error)
+    });
+  }
+
   //async api call
   performSearch = () => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.searchCity},${this.state.searchCountry}&APPID=36dbd4f4d180c0a6ea3f797fe7c0402d`)
